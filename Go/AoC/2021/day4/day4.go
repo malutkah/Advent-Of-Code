c7487b9992fde15e29a11b92fac700a0116383af
malutkah <73688226+malutkah@users.noreply.github.com>
2022-11-15 12:56:33 +0100

    stuff

---
+++ Go/AoC/2021/day4/day4.go
@@ -0,0 +1,14 @@
+package day4
+
+import (
+	aoc "AoC/Helper"
+	"fmt"
+)
+
+func Test() {
+	var inputs = aoc.GetInput("2021", "day4", "test")
+
+	for _, i := range inputs {
+		fmt.Println(i)
+	}
+}
---
+++ Go/CLI/Test/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 CLI Todo
+
+*/
+package main
+
+import "Todo/cmd"
+
+func main() {
+	cmd.Execute()
+}
