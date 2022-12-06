3c84fc213bd7a485614b71d0324a12722ad81e48
malutkah <alan.tressin@outlook.com>
2022-12-06 21:26:49 +0100

    day 4 done

--- Go/AoC/2022/day4/day4.go
+++ Go/AoC/2022/day4/day4.go
@@ -7,10 +7,12 @@ import (
 )
 
 func Solve() {
-	var inputs = aoc.GetInput("2022", "day4", "test")
+	var inputs = aoc.GetInput("2022", "day4", "input")
+	c := 0
+	o := 0
 
-	for _, i := range inputs {
-		sections := strings.Split(i, ",")
+	for _, l := range inputs {
+		sections := strings.Split(l, ",")
 
 		s1 := sections[0]
 		s2 := sections[1]
@@ -19,7 +21,10 @@ func Solve() {
 		pair1 := strings.Split(s1, "-")
 		id1 := aoc.StoI(pair1[0])
 		id2 := aoc.StoI(pair1[1])
+
 		// get range
+		range1 := []int{id1, id2}
+
 		for i := 0; i <= 9; i++ {
 			if i >= id1 && i <= id2 {
 				fmt.Printf("%d", i)
@@ -34,6 +39,8 @@ func Solve() {
 		id1 = aoc.StoI(pair2[0])
 		id2 = aoc.StoI(pair2[1])
 
+		range2 := []int{id1, id2}
+
 		for i := 0; i <= 9; i++ {
 			if i >= id1 && i <= id2 {
 				fmt.Printf("%d", i)
@@ -43,5 +50,21 @@ func Solve() {
 		}
 		fmt.Printf(": %v\n\n", pair2)
 
+		containing := (range1[0] >= range2[0] && range1[1] <= range2[1]) || (range2[0] >= range1[0] && range2[1] <= range1[1])
+		if containing {
+			c++
+		}
+
+		overlapping := (range1[0] <= range2[0] && range1[1] >= range2[0]) || (range2[0] <= range1[0] && range2[1] >= range1[0])
+
+		if overlapping {
+			o++
+		}
+
+		range1 = nil
+		range2 = nil
 	}
+
+	fmt.Printf("%d pairs fully contain the other\n\n", c)
+	fmt.Printf("%d overlapping pairs: \n\n", o)
 }
---
+++ Go/AoC/2022/day5/day5.go
@@ -0,0 +1,14 @@
+package day5
+
+import (
+	aoc "AoC/Helper"
+	"fmt"
+)
+
+func Solve() {
+    var inputs = aoc.GetInput("2022", "day5", "test")
+
+    for _, i := range inputs {
+        fmt.Println(i)
+    }
+}
